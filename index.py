84a65ffa1c1ec53b98cb74a584be66e6a65f5e7a
tolgabal <tolgabal752@gmail.com>
2025-03-01 01:14:07 +0300

    create main screen, add bg pic

--- index.py
+++ index.py
@@ -6,3 +6,11 @@ Created on Sat Mar  1 00:55:23 2025
 @author: tolgabal
 """
 
+import turtle
+
+main_screen = turtle.Screen()
+main_screen.bgpic("bgpic.gif")
+main_screen.screensize(600,338)
+main_screen.setup(600,338)
+
+turtle.mainloop()
