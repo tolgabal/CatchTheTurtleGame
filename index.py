e7176dcb544663068fca8d7c69a8554bacde617f
tolgabal <tolgabal752@gmail.com>
2025-03-01 01:43:19 +0300

    add test text, add turtle pic

--- index.py
+++ index.py
@@ -12,5 +12,16 @@ main_screen = turtle.Screen()
 main_screen.bgpic("bgpic.gif")
 main_screen.screensize(600,338)
 main_screen.setup(600,338)
+main_screen.addshape("turtlepic.gif")
+
+turtle_turtle = turtle.Turtle()
+turtle_turtle.shape("turtlepic.gif")
+
+turtle_writer = turtle.Turtle()
+turtle_writer.hideturtle()
+turtle_writer.penup()
+turtle_writer.goto(-40,128)
+turtle_writer.write("test yazısı", font=("helvetica", 18, "bold"))
+
 
 turtle.mainloop()
