03c9a3fa55eba2dd58247230e434d717531914de
tolgabal <tolgabal752@gmail.com>
2025-03-01 12:38:20 +0300

    add timer

--- index.py
+++ index.py
@@ -22,7 +22,12 @@ turtle_writer.hideturtle()
 turtle_writer.penup()
 turtle_writer.color("red")
 
+turtle_counter = turtle.Turtle()
+turtle_counter.penup()
+turtle_counter.hideturtle()
+
 score = 0
+counterVar = 16
 
 def scoreboard_writer(score):
     turtle_writer.clear()
@@ -33,6 +38,16 @@ def turtle_clicker(x,y):
     global score
     score +=1
     scoreboard_writer(score)
+    
+def counter():
+    global counterVar
+    turtle_counter.clear()
+    turtle_counter.goto(-10,98)
+    counterVar -= 1
+    turtle_counter.write(counterVar, font=("helvetica", 18, "bold"))
+    main_screen.ontimer(counter, 1000)
+    
+counter()
     
 scoreboard_writer(score)
 
