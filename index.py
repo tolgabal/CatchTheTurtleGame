64796d661c3bbae25280e07c6cfa00d545f0f5f4
tolgabal <tolgabal752@gmail.com>
2025-03-01 12:50:49 +0300

    add random teleport to turtle

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ Created on Sat Mar  1 00:55:23 2025
 """
 
 import turtle
+import random
 
 main_screen = turtle.Screen()
 main_screen.bgpic("bgpic.gif")
@@ -16,6 +17,7 @@ main_screen.addshape("turtlepic.gif")
 
 turtle_turtle = turtle.Turtle()
 turtle_turtle.shape("turtlepic.gif")
+turtle_turtle.penup()
 
 turtle_writer = turtle.Turtle()
 turtle_writer.hideturtle()
@@ -47,10 +49,19 @@ def counter():
     turtle_counter.write(counterVar, font=("helvetica", 18, "bold"))
     main_screen.ontimer(counter, 1000)
     
-counter()
+def turtle_teleport():
+    xPlane = random.randint(-300, 300)
+    yPlane = random.randint(-169, 169)
+    turtle_turtle.hideturtle()
+    turtle_turtle.goto(xPlane,yPlane)
+    turtle_turtle.showturtle()
     
+    main_screen.ontimer(turtle_teleport, 700)
+    
+turtle_teleport()
+counter()
 scoreboard_writer(score)
-
 turtle_turtle.onclick(turtle_clicker)
+
 
 turtle.mainloop()
